4114e30f1012876133564e248d889f14b8deb6b3
dkingma1 <dkingma1@studentmail.nait.ca>
2017-01-17 15:03:08 -0700

    Setup of Mvc Site

---
+++ MvcWepSite/MvcWepSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcWepSite.Startup))]
+namespace MvcWepSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
